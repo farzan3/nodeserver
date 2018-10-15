95720198feb54d975013ae70faecc3544476bb38
ALI FARZAN <ALI FARZAN@ALIFARZAN>
2018-10-15 02:03:59 -0700

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-
+const port = process.env.PORT || 3000;
 app.get('/scrape/:id', function(req, res){
  
   var id = req.params.id;
@@ -33,6 +33,6 @@ app.get('/scrape/:id', function(req, res){
   })
 })
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
+app.listen(port)
+  console.log('Server started on port', port);
 exports = module.exports = app;
