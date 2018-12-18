d3b138273696fccce0273f232cc55626a9c0a052
farzan3 <alifarzantahaa@gmail.com>
2018-12-18 15:13:37 +0500

    Add files via upload

---
+++ google-play-scraper/lib/utils/parseList.js
@@ -0,0 +1,86 @@
+'use strict';
+
+/*
+ * Return the proper parseList function according to the options.
+ */
+function getParseList (getApp, opts) {
+  if (opts.fullDetail) {
+    return getParseDetailList(getApp, opts);
+  }
+
+  return ($) => Promise.resolve(parseList($));
+}
+
+/*
+ * Returns a parseList function that just grabs the appIds,
+ * fetches every app detail with the app() function and returns
+ * a Promise.all().
+ */
+function getParseDetailList (getApp, opts) {
+  return function ($) {
+    const promises = $('.card').get().slice(0, opts.num).map(function (app) {
+      const appId = $(app).attr('data-docid');
+      return getApp({
+        appId: appId,
+        lang: opts.lang,
+        country: opts.country,
+        cache: opts.cache
+      });
+    });
+
+    return Promise.all(promises);
+  };
+}
+
+function parseList ($) {
+  // console.log($.html());
+  // const fs = require('fs');
+  // fs.writeFile("/tmp/ABC.html", $.html(), function(err) {
+  //     if(err) {
+  //         return console.log(err);
+  //     }
+
+  //     console.log("The file was saved!");
+  // }); 
+  // console.log($('.WHE7ib').get());
+  return $('.WHE7ib').get().map(function (app) {
+    // console.log($(app));
+    return parseApp($(app));
+  });
+}
+
+function splittoken(body, token)
+{
+  return body.substring(body.search(token)+token.length);
+   
+}
+
+function parseApp (app) {
+  // console.log(app);
+  let price = app.find('span.display-price').first().text();
+
+  // if price string contains numbers, it's not free
+  const free = !/\d/.test(price);
+
+  const scoreText = app.find('div.tiny-star').attr('aria-label');
+  let score;
+  if (scoreText) {
+    score = parseFloat(scoreText.match(/[\d.]+/)[0]);
+  }
+
+  return {
+    url: 'https://play.google.com' + app.find('a').attr('href'),
+    appId: splittoken(app.find('a').attr('href'), "id="),
+    title: app.find('div.b8cIId').attr('title'),
+     summary: 'sum',
+    developer: app.find('a.mnKHRc').text(),
+    developerId: splittoken(app.find('a.mnKHRc').attr('href'), "id="),
+     icon: '' ,
+     score: 0,
+     scoreText: 0,
+     priceText: 0,
+     free: false
+  };
+}
+
+module.exports = getParseList;
--- server.js
+++ server.js
@@ -1,32 +1,13 @@
-var express = require('express');
-var request = require('request');
-var app     = express();
+'use strict';
+
+const Express = require('express');
+const router = require('./lib');
+
+const app = Express();
 const port = process.env.PORT || 3000;
-app.get('/scrape/:id', function(req, res){
- 
-  var id = req.params.id;
-  url = 'http://mishnat.com/MishNat/appInfoApi.php?packageName='+id;
 
-  request({ url: url,json: true}, function(error, response, body){
-    if(!error){
-		
-	
-      
+app.use('/api/', router);
 
-	  var ranking="None";
-      
-      var json = { pakagenname : "None", url : "None" , bit :0};
-
-	  json.pakagenname=body.Package;
-		json.url=body.HeaderImage;
-		json.bit=1;
-	
-	}
-
-    res.send(json)
-  })
-})
-
-app.listen(port)
+app.listen(port, function () {
   console.log('Server started on port', port);
-exports = module.exports = app;
+});
